cef4c72cb7c70607bfe02f74e223ce032c78378e
nicochris-3500 <desertnicolas2000@gmail.com>
2026-01-27 11:28:54 +0100

    Update Basededonnee.sql

--- Basededonnee.sql
+++ Basededonnee.sql
@@ -5,5 +5,30 @@ Année int(4) NOT NULL,
 Détails varchar(40),
 Type varchar(20) NOT NULL,
 Appartenance varchar(30));
-
+Insert into MATERIEL_M2L( ID, Nom, Année, Détails, Type, Appartenace)
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(2, 'PC 2 – Unité centrale' ,2017,'','PC','')
+VALUES(3, 'PC 3 – Portable' ,2015,'	Inspiron 15-3558','PC','')
+VALUES(4, 'Écran A' ,2012,'HP LA1951g – 19’’ – 1280×1024 – 60 Hz','Ecran','')
+VALUES(5, 'Écran B	' ,2010,'Dell E178FP – 17’’ – 1280×1024','Ecran','')
+VALUES(6, 'Écran C' ,2009,'Samsung 933SN – 18.5’’ – 1366×768','Ecran','')
+VALUES(10, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
+VALUES(1, 'PC 1 – Unité centrale' ,2016,'','PC','')
 
