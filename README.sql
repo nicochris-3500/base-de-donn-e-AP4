fd99f011e50808967242903a50c919e1be6f9cfa
nicochris-3500 <desertnicolas2000@gmail.com>
2026-01-27 09:42:41 +0100

    Add MATERIEL_M2L table creation to README.sql

--- README.sql
+++ README.sql
@@ -1,1 +1,8 @@
 SELECT * FROM Customers;
+CREATE TABLE MATERIEL_M2L( 
+ID int(3) NOT NULL primary key,
+Nom VARCHAR(30) NOT NULL,
+Année int(4) NOT NULL,
+Détails varchar(40),
+Type varchar(20) NOT NULL,
+Appartenance varchar(30));
