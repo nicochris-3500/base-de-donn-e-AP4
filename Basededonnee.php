79d65bfa380bea665c083cda714ec9570cc68eed
Arth75 <arthur.leeguianvarch@gmail.com>
2026-02-17 09:37:55 +0100

    Refactor database connection and display ideas

--- Basededonnee.php
+++ Basededonnee.php
@@ -1,21 +1,56 @@
 <!DOCTYPE html>
-<html>
-<body>
- 
-<?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "mydb";
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Boîte à idées
+        </title>
+    </head>
+    <body>
+    <?php
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?>
-
-
-</body>
+            require('credentials.php');
+            $connexion = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $password);
+   
+            $requete = $connexion->prepare('select * from idea');
+            $requete->execute();
+            $idees = $requete->fetchAll();
+        ?>
+    <table>
+            <tr>
+                <th>
+                    Nom
+                </th>
+                <th>
+                    Texte
+                </th>
+            </tr>
+            <?php
+  
+                foreach ($idees as $idee)
+                {
+                    $url = "show.php?id=" . $idee['id'];
+                    ?>
+                    <tr>
+                        <td>
+                            <a href="<?php print($url) ?>">
+                                <?php print($idee['nom']) ?>
+                            </a>
+                        </td>
+                        <td>
+                            <?php print($idee['texte']) ?>
+                        </td>
+                    </tr>
+                    <?php
+                }
+            ?>
+        </table>
+        <form method="post" action="insert.php">
+            <input name="nom">
+            <br>
+            <textarea name="texte"></textarea>
+            <br>
+            <input type="submit" value="Ajouter une idée">
+        </form>
+    </body>
 </html>
