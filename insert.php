0e40a3364fab11f73bbb382d73a5810673b8c61c
Arth75 <arthur.leeguianvarch@gmail.com>
2026-02-17 11:13:09 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,7 +3,7 @@
     require('code.php');
     $connexion = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $password);
     
-    if (isset($_POST['nom']) || isset($_POST['texte']))
+    if (isset($_POST['ID']) || isset($_POST['Nom']) || isset($_POST['Année']) || isset($_POST['Détails']) || isset($_POST['Type']) || isset($_POST['Appartenance']))
     {
       $chaine = "insert into matériel_m2l (ID, Nom, Année, Détails, Type, Appartenance) values('" . $_POST['ID'] . "', '" . $_POST['Nom'] . "', '" . $_POST['Anné'] . "', '" . $_POST['Détails'] . "', '" . $_POST['Type'] . "', '" . $_POST['Appartenance'] . "')";
       $requete = $connexion->prepare($chaine);
