e52f2c039eeee1c4aa39584997db4fe62850e9b9
Arth75 <arthur.leeguianvarch@gmail.com>
2026-02-17 09:56:38 +0100

    Change insert query to use matériel_m2l table

--- insert.php
+++ insert.php
@@ -1,11 +1,11 @@
 <?php
     
-    require('credentials.php');
+    require('code.php');
     $connexion = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $password);
     
     if (isset($_POST['nom']) || isset($_POST['texte']))
     {
-      $chaine = "insert into idea (nom, texte) values('" . $_POST['nom'] . "', '" . $_POST['texte'] . "')";
+      $chaine = "insert into matériel_m2l (nom, texte) values('" . $_POST['nom'] . "', '" . $_POST['texte'] . "')";
       $requete = $connexion->prepare($chaine);
       $resultat = $requete->execute();
     }
