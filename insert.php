d1c1acfaef556d95e757a417ff6ebb19bf1dee7e
Arth75 <arthur.leeguianvarch@gmail.com>
2026-02-17 10:31:26 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,7 +5,7 @@
     
     if (isset($_POST['nom']) || isset($_POST['texte']))
     {
-      $chaine = "insert into matériel_m2l (ID, Nom, Année, Détails, Type, Appartenace) values('" . $_POST['nom'] . "', '" . $_POST['texte'] . "')";
+      $chaine = "insert into matériel_m2l (ID, Nom, Année, Détails, Type, Appartenace) values('" . $_POST['ID'] . "', '" . $_POST['Nom'] . "', '" . $_POST['Anné'] . "', '" . $_POST['Détails'] . "', '" . $_POST['Type'] . "', '" . $_POST['Appartenance'] . "')";
       $requete = $connexion->prepare($chaine);
       $resultat = $requete->execute();
     }
